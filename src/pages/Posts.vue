a88ee43c299473bfbf221026800f48d197634655
Arel <arel.hello@gmail.com>
2019-04-28 23:57:00 +0800

    fix: #4 - Add page for post listing

---
+++ src/pages/Posts.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <Layout>
+    <ul>
+      <li v-for="{ node } in $page.allPost.edges" :key="node._id">
+        <router-link :to="node.path">
+          <h2>{{ node.title }}</h2>
+        </router-link>
+        <span>{{ node.date }}</span>
+        <div v-html="node.desc" />
+      </li>
+    </ul>
+  </Layout>
+</template>
+
+<page-query>
+  query Posts ($page: Int) {
+    allPost (page: $page) {
+      edges {
+        node {
+          _id
+          title
+          date (format: "D MMMM, YYYY")
+          path
+        }
+      }
+    }
+  }
+</page-query>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  ul li {
+    margin-bottom: 20px;
+  }
+  ul li a h2 {
+    margin-bottom: 10px;
+  }
+  span {
+    font-size: 80%;
+    padding: 0;
+  }
+  ul li p:first-child {
+    margin-top: 3px;
+  }
+  ul li p {
+    margin: 0;
+    line-height: 1.5;
+  }
+</style>

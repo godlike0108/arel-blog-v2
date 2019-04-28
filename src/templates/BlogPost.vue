87c24d591a822c9522ab3979f378166c277b4db5
Arel <arel.hello@gmail.com>
2019-04-28 14:04:54 +0800

    Add the first blog template and mock data. (#1)

---
+++ src/templates/BlogPost.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout>
+    <h1>{{ $page.post.title }}</h1>
+    <div v-html="$page.post.content"/>
+  </Layout>
+</template>
+
+<page-query>
+query BlogPost ($path: String!) {
+  post: blogPost (path: $path) {
+    title
+    content
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.post.title
+    };
+  }
+};
+</script>
